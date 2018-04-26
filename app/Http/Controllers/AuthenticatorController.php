c3abd0285fd6f36bea9cd6e7f5d80c5eb5e80225
Fabricio Oliveira <fabricio.jhonata@gmail.com>
2018-04-25 22:05:47 -0300

    lista de usuarios

--- app/Http/Controllers/AuthenticatorController.php
+++ app/Http/Controllers/AuthenticatorController.php
@@ -16,6 +16,7 @@ class AuthenticatorController extends Controller
         ]);
     }
 
+
     public function code(Request $request){
         $code = $request->get('code');
 
--- routes/web.php
+++ routes/web.php
@@ -4,3 +4,7 @@ Route::get('/', "AuthenticatorController@index");
 Route::get('/code', "AuthenticatorController@code");
 
 Route::get('/monitor', "MonitorController@index");
+
+Route::get(function(){
+    dd(User::all());
+});
